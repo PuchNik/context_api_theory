b3a4c35224d4fffee38c4f9b7832186fdf5f1fdf
PuchNik <i.p.chn.k@gmail.com>
2025-03-11 16:22:23 +0300

    добавлен динамический контекст

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Header, UserBlock} from "./components/index.js"
 import {AppContextProvider} from "./contextAPI/AppContextProvider.jsx";
+import {useEffect, useState} from "react";
 
 
 const getUserFromServer = () => ({
@@ -18,10 +19,31 @@ const getProductFromServer = () => ({
     country: 'Russia'
 })
 
-export default function App() {
-    const userData = getUserFromServer()
+const getAnotherUserFromServer = () => ({
+    id: 'user_1',
+    name: 'Igor',
+    age: 23,
+    email: 'puch@gmail.com',
+    phone: '8-888-888-88-88',
+})
 
-    const productData = getProductFromServer()
+
+export default function App() {
+    const [userData, setUserData] = useState({})
+    const [productData, setProductData] = useState({})
+
+    useEffect(() => {
+        const userDataFromServer = getUserFromServer()
+        setUserData(userDataFromServer)
+
+        const productDataFromServer = getProductFromServer()
+        setProductData(productDataFromServer)
+    }, [])
+
+    const onUserChange = () => {
+        const anotherUserDataFromServer = getAnotherUserFromServer()
+        setUserData(anotherUserDataFromServer)
+    }
 
     return (
         <AppContextProvider userValue={userData} productValue={productData}>
@@ -29,6 +51,7 @@ export default function App() {
             <hr/>
 
             <UserBlock/>
+            <button onClick={onUserChange}>Сменить пользователя</button>
         </AppContextProvider>
     )
 }
